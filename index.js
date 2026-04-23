356a98d8909e4a7378bc21fe40258851d36414be
horibaryosukeflute <horiba.ryosuke.flute@gmail.com>
2026-04-23 11:01:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get("/", (req, res) => {
   res.send("サーバー起動OK");
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
